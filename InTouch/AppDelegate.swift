ece472f2f9521bfe52386ba7b30c7a4063e48ce9
Will <will@Williams-MacBook-Pro.local>
2021-02-26 19:39:24 -0500

    Added Back4App server configuration

--- InTouch/AppDelegate.swift
+++ InTouch/AppDelegate.swift
@@ -7,19 +7,43 @@
 
 import Foundation
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
-        print("Working!")
-        
+        let configuration = ParseClientConfiguration {
+            $0.applicationId = "FKGNGwBwmYtoLC3UdfNMicOgIGznGmUMnFdOujCl"
+            $0.clientKey = "TTVJEwJIWdOlnzVCE55DYqY9Cuy8haz73iAp5x5j"
+            $0.server = "https://parseapi.back4app.com"
+        }
+        Parse.initialize(with: configuration)
+        saveInstallationObject()
         return true
     }
-
+    
+    func saveInstallationObject(){
+        if let installation = PFInstallation.current(){
+            installation.saveInBackground {
+                (success: Bool, error: Error?) in
+                if (success) {
+                    print("You have successfully connected your app to Back4App!")
+                } else {
+                    if let myError = error{
+                        print(myError.localizedDescription)
+                    }else{
+                        print("Uknown error")
+                    }
+                }
+            }
+        }
+    }
+    
+    
     // MARK: - UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
